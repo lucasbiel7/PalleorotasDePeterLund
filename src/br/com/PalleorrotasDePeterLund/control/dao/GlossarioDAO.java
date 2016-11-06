66a03ca5b119b6f6495b15334607f7ad3e52fabc
lucasbiel7 <lucasbiel7@icloud.com>
2016-11-06 11:01:07 -0200

    Listagem do glossário foi ajustada

--- src/br/com/PalleorrotasDePeterLund/control/dao/GlossarioDAO.java
+++ src/br/com/PalleorrotasDePeterLund/control/dao/GlossarioDAO.java
@@ -7,11 +7,25 @@ package br.com.PalleorrotasDePeterLund.control.dao;
 
 import br.com.PalleorrotasDePeterLund.model.GenericaDAO;
 import br.com.PalleorrotasDePeterLund.model.entity.Glossario;
+import java.util.List;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
  * @author lucas
  */
 public class GlossarioDAO extends GenericaDAO<Glossario> {
+
+    public GlossarioDAO() {
+        criteria = criteria.addOrder(Order.asc("palavra"));
+    }
+
+    public List<Glossario> pegarPorPesquisa(String text) {
+        entitys = criteria.add(Restrictions.ilike("palavra", text, MatchMode.ANYWHERE)).list();
+        closeSession();
+        return entitys;
+    }
 
 }
--- src/br/com/PalleorrotasDePeterLund/view/GlossarioController.java
+++ src/br/com/PalleorrotasDePeterLund/view/GlossarioController.java
@@ -5,12 +5,27 @@
  */
 package br.com.PalleorrotasDePeterLund.view;
 
+import br.com.PalleorrotasDePeterLund.control.dao.GlossarioDAO;
+import br.com.PalleorrotasDePeterLund.model.entity.Area;
+import br.com.PalleorrotasDePeterLund.model.entity.Glossario;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 /**
  * FXML Controller class
@@ -24,13 +39,41 @@ public class GlossarioController implements Initializable {
 
     @FXML
     private TextField tfPesquisa;
-    
+    @FXML
+    private TextFlow tfContainer;
+    @FXML
+    private ScrollPane spContainer;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        carregarGlossario();
+    }
 
+    @FXML
+    private void tfPesquisaKeyEvent(KeyEvent keyEvent) {
+        carregarGlossario();
+    }
+
+    public void carregarGlossario() {
+        tfContainer.getChildren().clear();
+        List<Glossario> glossarios = new GlossarioDAO().pegarPorPesquisa(tfPesquisa.getText());
+        Map<Area, List<Glossario>> glossarioOrganizado = glossarios.stream().collect(Collectors.groupingBy(Glossario::getArea));
+        for (Area area : glossarioOrganizado.keySet()) {
+            Text lbArea = new Text(area.getNome() + "\n");
+            lbArea.setFont(Font.font(Font.getDefault().getFamily(), FontWeight.EXTRA_BOLD, 25));
+            tfContainer.getChildren().add(lbArea);
+            for (Glossario glossario : glossarioOrganizado.get(area)) {
+                Text lbPalavra = new Text(glossario.getPalavra() + ": ");
+                lbPalavra.setFont(Font.font(Font.getDefault().getFamily(), FontWeight.BOLD, 14));
+                Text lbSignificado = new Text(glossario.getSignificado() + "\n");
+                lbSignificado.setFont(Font.font(Font.getDefault().getFamily(), FontPosture.ITALIC, 14));
+                tfContainer.getChildren().addAll(lbPalavra, lbSignificado);
+            }
+            tfContainer.getChildren().add(new Text("\n\n"));
+        }
     }
 
 }
